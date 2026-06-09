0cd81a6d4a5427166d9286d21fd5683fa3d714ba
Pradnaya35 <pradnyapatil132494@gmail.com>
2026-06-10 02:34:58 +0530

    Add files via upload
    Day1

---
+++ HomeAssignment1.spec.ts
@@ -0,0 +1,24 @@
+let userName='Pradnya'
+console.log(userName);
+console.log(typeof userName);
+
+let organization='Testleaf'
+console.log(organization);
+console.log(typeof organization);
+
+let contactNumber=9999999999
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+let knowsAutomation= false
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
+
